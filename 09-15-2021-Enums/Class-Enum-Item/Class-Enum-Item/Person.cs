0ce433a6ed708b7d4c1eb1c7a812a8a9e22e818a
Jacob-Dunbar-CS <jacob.a.dunbar@outlook.com>
2021-09-15 20:14:14 -0600

    setup classes for obect demo, create enum definition

---
+++ 09-15-2021-Enums/Class-Enum-Item/Class-Enum-Item/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Enum_Item.Data
+{
+    class Person
+    {
+        //Each insantace of this class will represent an individual
+        //This class will define the following characteristics
+        //  First Name, Last Name, list of employment positions
+    }
+}
